68020de335cf511c5e1f341d32f246d191caa30e
phendges7 <uk_hendges@outlook.com>
2025-04-11 23:23:01 +0100

    update CORS permission

--- backend/app.js
+++ backend/app.js
@@ -54,7 +54,7 @@ import {
 } from "./validators/usersValidator.js";
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://web-project-api-full-ochre.vercel.app/" }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
 
